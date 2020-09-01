5c8cf3697422bf8c302c3ca02c77358c5b78e5b7
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-09-01 10:06:38 -0500

    first commit

---
+++ KidPoems.sql
@@ -0,0 +1,11 @@
+﻿--KID POEM EXERCISE--
+/*     EXERCISE 1    */
+--SELECT * FROM grade;
+
+/*     EXERCISE 2    */
+--SELECT name from Emotion;
+
+/*     EXERCISE 3    */
+--SELECT COUNT(id) from Poem;
+
+/*     EXERCISE 4    */
